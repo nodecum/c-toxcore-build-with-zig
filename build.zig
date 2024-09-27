aa02c979b6beba5c8a32f95b0f022def1c039a8b
robert y <6729524+freylax@users.noreply.github.com>
2024-09-27 09:39:05 +0200

    initial setup

---
+++ build.zig
@@ -0,0 +1,105 @@
+const std = @import("std");
+const params = @import("src/params.zig");
+// Although this function looks imperative, note that its job is to
+// declaratively construct a build graph that will be executed by an external
+// runner.
+pub fn build(b: *std.Build) void {
+    // Standard target options allows the person running `zig build` to choose
+    // what target to build for. Here we do not override the defaults, which
+    // means any target is allowed, and the default is native. Other options
+    // for restricting supported target set are available.
+    const target = b.standardTargetOptions(.{});
+
+    // Standard optimization options allow the person running `zig build` to select
+    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall. Here we do not
+    // set a preferred release mode, allowing the user to decide how to optimize.
+    const optimize = b.standardOptimizeOption(.{});
+
+    const toxcore_dep = b.dependency("c-toxcore", .{ .target = target, .optimize = optimize });
+
+    const lib = b.addStaticLibrary(.{
+        .name = "build-c-toxcore-with-zig",
+        // In this case the main source file is merely a path, however, in more
+        // complicated build scripts, this could be a generated file.
+        .root_source_file = b.path("src/root.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    // This declares intent for the library to be installed into the standard
+    // location when the user invokes the "install" step (the default step when
+    // running `zig build`).
+    b.installArtifact(lib);
+
+    // we have to read the CMakeLists.txt file
+    // to achieve this we copy the dependency to
+    // tmp/CMakeLists.txt
+    const CMakeLists_orig = toxcore_dep.path("CMakeLists.txt");
+    const CMakeLists_install = b.addInstallFileWithDir(CMakeLists_orig, .{ .custom = "tmp" }, "CMakeLists.txt");
+
+    const extract_build_params = b.addExecutable(.{
+        .name = "extract_build_params",
+        .root_source_file = b.path("src/extract_build_params.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    // This declares intent for the executable to be installed into the
+    // standard location when the user invokes the "install" step (the default
+    // step when running `zig build`).
+    b.installArtifact(extract_build_params);
+
+    // This *creates* a Run step in the build graph, to be executed when another
+    // step is evaluated that depends on it. The next line below will establish
+    // such a dependency.
+    const run_extract = b.addRunArtifact(extract_build_params);
+
+    run_extract.addArg("--cmake-lists");
+    run_extract.addArg("zig-out/tmp/CMakeLists.txt");
+    run_extract.addArg("--params-zig");
+    run_extract.addArg("src/params.zig");
+
+    run_extract.step.dependOn(&CMakeLists_install.step);
+    // By making the run step depend on the install step, it will be run from the
+    // installation directory rather than directly from within the cache directory.
+    // This is not necessary, however, if the application depends on other installed
+    // files, this ensures they will be present and in the expected location.
+    run_extract.step.dependOn(b.getInstallStep());
+
+    // This allows the user to pass arguments to the application in the build
+    // command itself, like this: `zig build run -- arg1 arg2 etc`
+    //    if (b.args) |args| {
+    //        run_cmd.addArgs(args);
+    //    }
+
+    // This creates a build step. It will be visible in the `zig build --help` menu,
+    // and can be selected like this: `zig build run`
+    // This will evaluate the `run` step rather than the default, which is "install".
+    const run_step = b.step("run", "Run the app");
+    run_step.dependOn(&run_extract.step);
+
+    // Creates a step for unit testing. This only builds the test executable
+    // but does not run it.
+    const lib_unit_tests = b.addTest(.{
+        .root_source_file = b.path("src/root.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
+
+    const exe_unit_tests = b.addTest(.{
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
+
+    // Similar to creating the run step earlier, this exposes a `test` step to
+    // the `zig build --help` menu, providing a way for the user to request
+    // running the unit tests.
+    const test_step = b.step("test", "Run unit tests");
+    test_step.dependOn(&run_lib_unit_tests.step);
+    test_step.dependOn(&run_exe_unit_tests.step);
+}
---
+++ src/extract_build_params.zig
@@ -0,0 +1,110 @@
+const std = @import("std");
+const usage =
+    \\Usage: ./extract_build_params [options]
+    \\
+    \\Options:
+    \\  --cmake-lists CMakeLists.txt_FILE
+    \\  --params-zig params.zig_FILE
+    \\
+;
+
+const State = enum {
+    lines,
+    sources,
+};
+
+pub fn main() !void {
+    var arena_state = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena_state.deinit();
+    const arena = arena_state.allocator();
+
+    const args = try std.process.argsAlloc(arena);
+
+    var opt_in_file_path: ?[]const u8 = null;
+    var opt_out_file_path: ?[]const u8 = null;
+    {
+        var i: usize = 1;
+        while (i < args.len) : (i += 1) {
+            const arg = args[i];
+            if (std.mem.eql(u8, "-h", arg) or std.mem.eql(u8, "--help", arg)) {
+                try std.io.getStdOut().writeAll(usage);
+                return std.process.cleanExit();
+            } else if (std.mem.eql(u8, "--cmake-lists", arg)) {
+                i += 1;
+                if (i > args.len) fatal("expected arg after '{s}'", .{arg});
+                if (opt_in_file_path != null) fatal("duplicated {s} argument", .{arg});
+                opt_in_file_path = args[i];
+            } else if (std.mem.eql(u8, "--params-zig", arg)) {
+                i += 1;
+                if (i > args.len) fatal("expected arg after '{s}'", .{arg});
+                if (opt_out_file_path != null) fatal("duplicated {s} argument", .{arg});
+                opt_out_file_path = args[i];
+            } else {
+                fatal("unrecognized arg: '{s}', i:{d}, args.len:{d}", .{ arg, i, args.len });
+            }
+        }
+    }
+
+    const in_file_path = opt_in_file_path orelse fatal("missing --cmake-lists", .{});
+    const out_file_path = opt_out_file_path orelse fatal("missing --params-zig", .{});
+    // allocate a large enough buffer to store the cwd
+    var buf: [std.fs.max_path_bytes]u8 = undefined;
+
+    // getcwd writes the path of the cwd into buf and returns a slice of buf with the len of cwd
+    const cwd = try std.process.getCwd(&buf);
+
+    // print out the cwd
+    //    std.debug.print("cwd: {s}\n", .{cwd});
+    //std.debug.print("input file:{s}\n", .{in_file_path});
+    var in_file = std.fs.cwd().openFile(in_file_path, .{}) catch |err| {
+        fatal("unable to open '{s}': {s}, cwd:{s}", .{ in_file_path, @errorName(err), cwd });
+    };
+    defer in_file.close();
+    var out_file = std.fs.cwd().createFile(out_file_path, .{}) catch |err| {
+        fatal("unable to open '{s}': {s}, cwd:{s}", .{ out_file_path, @errorName(err), cwd });
+    };
+    defer out_file.close();
+
+    var buf_reader = std.io.bufferedReader(in_file.reader());
+    const reader = buf_reader.reader();
+
+    const writer = out_file.writer();
+
+    var line = std.ArrayList(u8).init(arena);
+    defer line.deinit();
+
+    const line_writer = line.writer();
+    var line_no: usize = 0;
+
+    var state = State.lines;
+    while (reader.streamUntilDelimiter(line_writer, '\n', null)) {
+        // Clear the line so we can reuse it.
+        defer line.clearRetainingCapacity();
+        line_no += 1;
+        //
+        if (state == State.lines) {
+            if (std.mem.eql(u8, line.items, "set(toxcore_SOURCES")) {
+                state = State.sources;
+                try writer.writeAll("const c_sources = [_][]const u8 {\n");
+            }
+        } else if (state == State.sources) {
+            var trimmed = std.mem.trim(u8, line.items, " \t,");
+            if (std.mem.endsWith(u8, trimmed, ")")) {
+                trimmed = trimmed[0 .. trimmed.len - 1];
+                state = State.lines; // end of sources
+            }
+            if (std.mem.endsWith(u8, trimmed, ".c")) {
+                try writer.print("    \"{s}\",\n", .{trimmed});
+                //std.debug.print("{d}--{s}\n", .{ line_no, trimmed });
+            }
+            if (state == State.lines) try writer.writeAll("};\n");
+        }
+    } else |err| switch (err) {
+        error.EndOfStream => {}, // end of file
+        else => return err, // Propagate error
+    }
+}
+fn fatal(comptime format: []const u8, args: anytype) noreturn {
+    std.debug.print(format, args);
+    std.process.exit(1);
+}

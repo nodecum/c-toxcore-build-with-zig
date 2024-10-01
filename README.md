8ce54ef84294c24e20084bce71501b51c82b09cf
robert y <6729524+freylax@users.noreply.github.com>
2024-10-01 23:09:39 +0200

    prepare for name change

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# build-c-toxcore-with-zig
+# c-toxcore-build-with-zig
+The intension of this repository is to provide a zig build file for the
+c-toxcore library. The usual way of doing this is to have build.zig in the
+root of the library. To add no further dependencies to the back of the c-toxcore
+developers this repository exists to be a proxy to the c-toxcore library.
+
+## build the static library
+> zig build install
+
+If a new version should be used, src/params.zig which contains the
+source files to be used can be updated by running
+
+> zig build run
+
+which pulls the necessary c-toxcore sources. 
--- build.zig
+++ build.zig
@@ -5,10 +5,7 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const c_toxcore_dep = b.dependency("c-toxcore", .{
-        //        .target = target,
-        //        .optimize = optimize,
-    });
+    const c_toxcore_dep = b.dependency("c-toxcore", .{});
 
     const libsodium_dep = b.dependency(
         "libsodium",
@@ -17,21 +14,9 @@ pub fn build(b: *std.Build) void {
     const libsodium = libsodium_dep.artifact("sodium");
     //    const gtest_dep = b.dependency("gtest", .{ .target = target, .optimize = optimize });
     const cmp_dep = b.dependency("cmp", .{});
-    // we copy the third_party dependencies into the source tree
-    // not an ideal solution but we need them there and git
-    // submodule want work for zig dependency snapshots.
-    //    const copy_files = b.addWriteFiles();
-    //    _ = copy_files.addCopyFileToSource(
-    //        .{ .dependency = .{ .dependency = cmp_dep, .sub_path = "cmp.c" } },
-    //        "third_party/cmp/cmp.c",
-    //    );
-    //    _ = copy_files.addCopyFileToSource(
-    //        .{ .dependency = .{ .dependency = cmp_dep, .sub_path = "cmp.h" } },
-    //        "third_party/cmp/cmp.h",
-    //    );
 
     const lib = b.addStaticLibrary(.{
-        .name = "build-c-toxcore-with-zig",
+        .name = "c-toxcore-build-with-zig",
         .target = target,
         .optimize = optimize,
         .link_libc = true,
@@ -52,14 +37,9 @@ pub fn build(b: *std.Build) void {
         }
     }
     lib.addIncludePath(b.path("zig-out/tmp/toxcore"));
-    //lib.addIncludePath(curl_dep.path("include"));   // This declares intent for the library to be installed into the standard
-    lib.installHeader(b.path("zig-out/tmp/toxcore/tox.h"), "tox.h");
     lib.installHeadersDirectory(b.path("zig-out/tmp/toxcore"), "toxcore", .{ .exclude_extensions = &.{".c"} });
     lib.linkLibrary(libsodium);
     b.installArtifact(lib);
-
-    //lib.addCSourceFiles(.{ .files = params.c_sources });
-    //lib.step.dependOn(&copy_files.step);
 
     // we have to read the CMakeLists.txt file
     // to achieve this we copy the dependency to
@@ -83,8 +63,6 @@ pub fn build(b: *std.Build) void {
     run_extract.addArg("src/params.zig");
 
     run_extract.step.dependOn(&CMakeLists_install.step);
-    //run_extract.step.dependOn(b.getInstallStep());
-    lib.step.dependOn(&run_extract.step);
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_extract.step);
 }

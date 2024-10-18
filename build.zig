828dbcc428cbf5e559687f1cce3b84eabb3a185e
robert y <6729524+freylax@users.noreply.github.com>
2024-10-19 00:09:14 +0200

    made the update process more konform to the zig build system

--- build.zig
+++ build.zig
@@ -21,7 +21,6 @@ pub fn build(b: *std.Build) void {
     const toxcore_dir = wf.addCopyDirectory(c_toxcore_dep.path("toxcore"), "toxcore", include_h_c);
     _ = wf.addCopyDirectory(c_toxcore_dep.path("toxencryptsave"), "toxencryptsave", include_h_c);
     _ = wf.addCopyDirectory(cmp_dep.path(""), "third_party/cmp", include_h_c);
-    //_ = wf.addCopyFile(c_toxcore_dep.path("CMakeLists.txt"),"CMakeLists.txt");
     const root = wf.getDirectory();
 
     const lib = b.addStaticLibrary(.{
@@ -58,7 +57,7 @@ pub fn build(b: *std.Build) void {
 
     const entrypoint = tox_zig.getOutput();
 
-    // build cimgui as a module with the header file as the entrypoint
+    // build c-toxcore as a module with the header file as the entrypoint
     const mod_c_toxcore = b.addModule("c-toxcore", .{
         .root_source_file = entrypoint,
         .target = target,
@@ -68,27 +67,24 @@ pub fn build(b: *std.Build) void {
     mod_c_toxcore.linkLibrary(lib);
 
     // we have to read the CMakeLists.txt file
-    // to achieve this we copy the dependency to
-    // tmp/CMakeLists.txt
-    const CMakeLists_orig = c_toxcore_dep.path("CMakeLists.txt");
-    const CMakeLists_install = b.addInstallFileWithDir(CMakeLists_orig, .{ .custom = "tmp" }, "CMakeLists.txt");
+    const CMakeLists = c_toxcore_dep.path("CMakeLists.txt");
 
     const extract_build_params = b.addExecutable(.{
         .name = "extract_build_params",
         .root_source_file = b.path("src/extract_build_params.zig"),
-        .target = target,
-        .optimize = optimize,
+        .target = b.host,
     });
 
     b.installArtifact(extract_build_params);
     const run_extract = b.addRunArtifact(extract_build_params);
 
     run_extract.addArg("--cmake-lists");
-    run_extract.addArg("zig-out/tmp/CMakeLists.txt");
+    run_extract.addFileArg(CMakeLists);
     run_extract.addArg("--params-zig");
-    run_extract.addArg("src/params.zig");
+    const params_zig = run_extract.addOutputFileArg("params.zig");
+    const copy_params_zig = b.addUpdateSourceFiles();
+    copy_params_zig.addCopyFileToSource(params_zig, "src/params.zig");
 
-    run_extract.step.dependOn(&CMakeLists_install.step);
-    const run_step = b.step("update", "Update C sources");
-    run_step.dependOn(&run_extract.step);
+    const run_step = b.step("update", "Update the src/params.zig file wich contains the C sources which have to be compiled");
+    run_step.dependOn(&copy_params_zig.step);
 }
